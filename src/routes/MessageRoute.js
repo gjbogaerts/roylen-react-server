3eb63f456c975571677498a81eecaf118d66d94b
GJ Bogaerts <gj@raker.nl>
2020-02-08 22:02:15 +0100

    ReadMessage API + messagecount

--- src/routes/MessageRoute.js
+++ src/routes/MessageRoute.js
@@ -4,6 +4,29 @@ const gatekeeper = require('../middlewares/gatekeeper');
 const router = express.Router();
 const Message = mongoose.model('Message');
 
+router.get('/api/message/:userId', gatekeeper, async (req, res) => {
+	if (req.user._id != req.params.userId) {
+		res.status(422).send('You are not authorized to get these messages');
+	} else {
+		try {
+			Message.find({ toId: req.user._id }, (err, doc) => {
+				if (err) {
+					return res
+						.status(422)
+						.send('Something went wrong trying to pick up your messages.');
+				}
+				res.status(200).send(doc);
+			});
+		} catch (err) {
+			res
+				.status(422)
+				.send(
+					'Something went wrong when we tried to pick up your messages. Please try again later.'
+				);
+		}
+	}
+});
+
 router.post('/api/message', gatekeeper, async (req, res) => {
 	const { senderName, message, fromId, toId, adId, adTitle } = req.body;
 
