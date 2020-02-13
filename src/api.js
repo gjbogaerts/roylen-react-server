6bc4b62381f869f5c58c724d78e6cc0febebd62e
GJ Bogaerts <gj@raker.nl>
2020-02-13 17:30:05 +0100

    Feature: changed cros-policy

--- src/api.js
+++ src/api.js
@@ -31,6 +31,14 @@ app.get('/', (req, res) => {
 });
 
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
 app.use(authRoutes);
 app.use(adRoutes);
 app.use(msgRoutes);
