75eea5fa0c4708ec01e827bbdd2c40978ab5678d
GJ Bogaerts <gj@raker.nl>
2020-02-15 10:08:31 +0100

    Feature: Secure admin access through JWT
    Admin access now only possible  with  Auth Header and Bearer JWT token.

--- src/routes/AdminRoute.js
+++ src/routes/AdminRoute.js
@@ -5,11 +5,15 @@ const User = mongoose.model('User');
 const router = express.Router();
 const ADMIN = require('../env/admin');
 
+const jwt = require('jsonwebtoken');
+const JWTKey = require('../env/keys');
+const adminSecure = require('../middlewares/adminkeeper');
+
 const sendErr = (res, err) => {
 	return res.status(422).send({ error: 1, message: err });
 };
 
-router.post('/api/admin/user/ban', (req, res) => {
+router.post('/api/admin/user/ban', adminSecure, (req, res) => {
 	const { id } = req.body;
 	try {
 		User.updateOne({ _id: id }, { isBanned: true }, (err, doc) => {
@@ -21,7 +25,7 @@ router.post('/api/admin/user/ban', (req, res) => {
 	}
 });
 
-router.post('/api/admin/user/unban', (req, res) => {
+router.post('/api/admin/user/unban', adminSecure, (req, res) => {
 	const { id } = req.body;
 	try {
 		User.updateOne({ _id: id }, { isBanned: false }, (err, doc) => {
@@ -33,17 +37,7 @@ router.post('/api/admin/user/unban', (req, res) => {
 	}
 });
 
-router.post('/api/admin/auth', (req, res) => {
-	const adminString = JSON.stringify(ADMIN);
-	const reqString = JSON.stringify(req.body);
-	if (adminString === reqString) {
-		res.status(200).send({ result: 1 });
-	} else {
-		res.status(422).send({ result: 0 });
-	}
-});
-
-router.post('/api/admin/ads/forbid', async (req, res) => {
+router.post('/api/admin/ads/forbid', adminSecure, async (req, res) => {
 	const { id } = req.body;
 	try {
 		Ad.updateOne({ _id: id }, { isForbidden: true }, (err, doc) => {
@@ -61,7 +55,7 @@ router.post('/api/admin/ads/forbid', async (req, res) => {
 	}
 });
 
-router.post('/api/admin/ads/unforbid', async (req, res) => {
+router.post('/api/admin/ads/unforbid', adminSecure, async (req, res) => {
 	const { id } = req.body;
 	try {
 		Ad.updateOne({ _id: id }, { isForbidden: false }, (err, doc) => {
@@ -72,8 +66,20 @@ router.post('/api/admin/ads/unforbid', async (req, res) => {
 		res
 			.status(422)
 			.send(
-				'Error torying to update the ad. Please use the command line directly or try later.'
+				'Error trying to update the ad. Please use the command line directly or try later.'
 			);
+	}
+});
+
+router.post('/api/admin/auth', (req, res) => {
+	const adminString = JSON.stringify(ADMIN);
+	const reqString = JSON.stringify(req.body);
+	const adminName = req.body.fn;
+	if (adminString === reqString) {
+		const token = jwt.sign({ name: adminName }, JWTKey);
+		res.status(200).send({ result: 1, token });
+	} else {
+		res.status(422).send({ result: 0 });
 	}
 });
 
