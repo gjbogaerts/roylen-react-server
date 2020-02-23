42176a98d71176a8e51c7b941c66de5392377b4d
GJ Bogaerts <gj@raker.nl>
2020-02-23 13:43:43 +0100

    Replaced 8 char limit on screenname with 3 char limit

--- src/models/User.js
+++ src/models/User.js
@@ -2,71 +2,71 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
-	screenName: {
-		type: String,
-		unique: true,
-		required: true,
-		minlength: 8
-	},
-	email: {
-		type: String,
-		unique: true,
-		required: true
-	},
-	password: {
-		type: String,
-		required: true
-	},
-	nix: {
-		type: Number,
-		default: 100,
-		required: true
-	},
-	pwResetKey: {
-		type: String
-	},
-	avatar: {
-		type: String
-	},
-	isBanned: {
-		type: Boolean,
-		default: false
-	},
-	ads: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Ad' }]
+  screenName: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  nix: {
+    type: Number,
+    default: 100,
+    required: true
+  },
+  pwResetKey: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  isBanned: {
+    type: Boolean,
+    default: false
+  },
+  ads: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Ad' }]
 });
 
 UserSchema.pre('save', function(next) {
-	const user = this;
-	if (!user.isModified('password')) {
-		return next;
-	}
-	bcrypt.genSalt(10, (err, salt) => {
-		if (err) {
-			return next(err);
-		}
-		bcrypt.hash(user.password, salt, (err, hash) => {
-			if (err) {
-				return next(err);
-			}
-			user.password = hash;
-			next();
-		});
-	});
+  const user = this;
+  if (!user.isModified('password')) {
+    return next;
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 UserSchema.methods.comparePassword = function(candidatePassword) {
-	const user = this;
-	return new Promise((resolve, reject) => {
-		bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
-			if (err) {
-				return reject(err);
-			}
-			if (!isMatch) {
-				return reject(false);
-			}
-			return resolve(true);
-		});
-	});
+  const user = this;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+      if (!isMatch) {
+        return reject(false);
+      }
+      return resolve(true);
+    });
+  });
 };
 
 mongoose.model('User', UserSchema);
