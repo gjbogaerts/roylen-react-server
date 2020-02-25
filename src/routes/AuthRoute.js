e1b8f17f0bcc7669b16f0a461282c1ec13298d49
GJ Bogaerts <gj@raker.nl>
2020-02-25 22:05:59 +0100

    New API calls for testing screenname and email

--- src/routes/AuthRoute.js
+++ src/routes/AuthRoute.js
@@ -13,129 +13,147 @@ sgMail.setApiKey(SendGridKey);
 const uploadURI = '/uploads/pics';
 const upload = require('../utils/uploads');
 
+router.get('/api/checkEmail/:email', async (req, res) => {
+  try {
+    User.find({ email: req.params.email }, '_id', (err, doc) => {
+      if (err) return res.status(422).send({ err: err.message });
+      return res.status(200).send({ numUsers: doc.length });
+    });
+  } catch (err) {
+    res.status(422).send({ err: err.message });
+  }
+});
+
+router.get('/api/checkScreenName/:screenName', async (req, res) => {
+  try {
+    User.find({ screenName: req.params.screenName }, '_id', (err, doc) => {
+      if (err) return res.status(422).send({ err: err.message });
+      return res.status(200).send({ numUsers: doc.length });
+    });
+  } catch (err) {
+    res.status(422).send({ err: err.message });
+  }
+});
+
 router.post('/api/signup', async (req, res) => {
-	const { email, password, screenName } = req.body;
-	try {
-		const user = new User({ email, password, screenName, nix: 100 });
-		await user.save();
-		const token = jwt.sign({ userId: user._id }, JWTKey);
-		res.send({ ...user.toJSON(), token });
-	} catch (e) {
-		console.log(e);
-		return res.status(422).send({ error: e.message });
-	}
+  const { email, password, screenName } = req.body;
+  try {
+    const user = new User({ email, password, screenName, nix: 100 });
+    await user.save();
+    const token = jwt.sign({ userId: user._id }, JWTKey);
+    res.send({ ...user.toJSON(), token });
+  } catch (e) {
+    console.log(e);
+    return res.status(422).send({ error: e.message });
+  }
 });
 
 router.post('/api/conformResetPassword', async (req, res) => {
-	const pwResetKey = req.body.key;
-	const password = req.body.pw;
-	try {
-		const user = await User.findOne({ pwResetKey });
-		if (!user) {
-			return res.status(422).send({ error: 'No user found with this key' });
-		}
-		user.password = password;
-		user.pwResetKey = '';
-		user.save({}, (err, doc) => {
-			if (err) return res.status(422).send({ error: err });
-			return res.status(200).send({
-				success: 1,
-				msg:
-					'Your password has been reset. You can now login with your new password',
-				doc
-			});
-		});
-	} catch (err) {
-		res.status(422).send({ error: err.message });
-	}
+  const pwResetKey = req.body.key;
+  const password = req.body.pw;
+  try {
+    const user = await User.findOne({ pwResetKey });
+    if (!user) {
+      return res.status(422).send({ error: 'No user found with this key' });
+    }
+    user.password = password;
+    user.pwResetKey = '';
+    user.save({}, (err, doc) => {
+      if (err) return res.status(422).send({ error: err });
+      return res.status(200).send({
+        success: 1,
+        msg:
+          'Your password has been reset. You can now login with your new password',
+        doc
+      });
+    });
+  } catch (err) {
+    res.status(422).send({ error: err.message });
+  }
 });
 
 router.post('/api/resetPassword', async (req, res) => {
-	const { email } = req.body;
-	// console.log(req.body);
-	const secretKey = crypto.randomBytes(20).toString('hex');
-	try {
-		const user = User.updateOne(
-			{ email },
-			{ pwResetKey: secretKey },
-			(err, doc) => {
-				if (err) return res.status(422).send({ error: err.message });
-				if (doc.nModified === 1) {
-					const msg = {
-						to: email,
-						from: 'do-not-reply@roylen.ga',
-						subject: 'Reset your password',
-						text: `Dear Roylen-user,\n\nsomebody, maybe you, has requested a new password to access the Roylen app. Please open the app, click the 'reset password' button on the login-page, and use the key provided here to reset your password.\n\n__________________________\n\nKey:${secretKey}\n\n__________________________\n\nTake care, you can only use this key once! \n\nWith kind regards,\nThe Roylen Team\n\nPS: you didn't ask for your password to reset? You don't need to do anything, your account is safe.\nPPS: You can't reply to this mail; your reply will get lost in the great empty void of bits and data on the internet...`,
-						html: `<p>Dear Roylen-user,</p><p>Somebody, maybe you, has requested a new password to access the Roylen app. Please open the app, click the 'reset password' button on the login-page, and use the key provided here to reset your password.</p><p><hr /><p>Key:<br />${secretKey}</p><hr /><p>Take care, you can only use this key once!</p><p>With kind regards,</p><p>The Roylen Team</p><p>PS: you didn't ask for your password to reset? You don't need to do anything, your account is safe.</p><p>PPS: You can't reply to this mail; your reply will get lost in the great empty void of bits and data on the internet...</p>`
-					};
-					sgMail.send(msg, false, (err, result) => {
-						if (err) return res.status(422).send({ error: err });
-						return res.status(200).send({
-							result,
-							success: 1,
-							msg:
-								"We have sent you a secret key to reset your password. If you don't see it, please check your spambox."
-						});
-					});
-				} else {
-					return res.status(200).send({
-						error:
-							"Sorry, we're unable to provide you with a password reset key. Please check the spelling of your email address."
-					});
-				}
-			}
-		);
-	} catch (e) {
-		return res.status(422).send({ error: e.message });
-	}
+  const { email } = req.body;
+  // console.log(req.body);
+  const secretKey = crypto.randomBytes(20).toString('hex');
+  try {
+    User.updateOne({ email }, { pwResetKey: secretKey }, (err, doc) => {
+      if (err) return res.status(422).send({ error: err.message });
+      if (doc.nModified === 1) {
+        const msg = {
+          to: email,
+          from: 'do-not-reply@roylen.ga',
+          subject: 'Reset your password',
+          text: `Dear Roylen-user,\n\nsomebody, maybe you, has requested a new password to access the Roylen app. Please open the app, click the 'reset password' button on the login-page, and use the key provided here to reset your password.\n\n__________________________\n\nKey:${secretKey}\n\n__________________________\n\nTake care, you can only use this key once! \n\nWith kind regards,\nThe Roylen Team\n\nPS: you didn't ask for your password to reset? You don't need to do anything, your account is safe.\nPPS: You can't reply to this mail; your reply will get lost in the great empty void of bits and data on the internet...`,
+          html: `<p>Dear Roylen-user,</p><p>Somebody, maybe you, has requested a new password to access the Roylen app. Please open the app, click the 'reset password' button on the login-page, and use the key provided here to reset your password.</p><p><hr /><p>Key:<br />${secretKey}</p><hr /><p>Take care, you can only use this key once!</p><p>With kind regards,</p><p>The Roylen Team</p><p>PS: you didn't ask for your password to reset? You don't need to do anything, your account is safe.</p><p>PPS: You can't reply to this mail; your reply will get lost in the great empty void of bits and data on the internet...</p>`
+        };
+        sgMail.send(msg, false, (err, result) => {
+          if (err) return res.status(422).send({ error: err });
+          return res.status(200).send({
+            result,
+            success: 1,
+            msg:
+              "We have sent you a secret key to reset your password. If you don't see it, please check your spambox."
+          });
+        });
+      } else {
+        return res.status(200).send({
+          error:
+            "Sorry, we're unable to provide you with a password reset key. Please check the spelling of your email address."
+        });
+      }
+    });
+  } catch (e) {
+    return res.status(422).send({ error: e.message });
+  }
 });
 
 router.post('/api/signin', async (req, res) => {
-	const { email, password } = req.body;
-	if (!email || !password) {
-		return send422(res);
-	}
-	const user = await User.findOne({ email });
-	if (!user) {
-		return send422(res);
-	}
-	try {
-		await user.comparePassword(password);
-		const token = jwt.sign({ userId: user._id }, JWTKey);
-		res.send({ ...user.toJSON(), token });
-	} catch (err) {
-		return send422(res);
-	}
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return send422(res);
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    return send422(res);
+  }
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, JWTKey);
+    res.send({ ...user.toJSON(), token });
+  } catch (err) {
+    return send422(res);
+  }
 });
 
 router.post('/api/profile', upload.any(), gatekeeper, async (req, res) => {
-	try {
-		let imagePath = null;
-		if (req.files && req.files.length > 0) {
-			imagePath = uploadURI + '/' + req.files[0]['filename'];
-		}
-		let email = req.user.email;
-		if (req.body['email'] != '') {
-			email = req.body['email'];
-		}
-		const q = { _id: req.user._id };
-		const newData = { email, avatar: imagePath };
-		User.findByIdAndUpdate(
-			q,
-			newData,
-			{ useFindAndModify: false },
-			(err, doc) => {
-				if (err) return send422(res);
-				return res.send({ success: 1, avatar: doc.avatar });
-			}
-		);
-	} catch (err) {
-		return send422(res);
-	}
+  try {
+    let imagePath = null;
+    if (req.files && req.files.length > 0) {
+      imagePath = uploadURI + '/' + req.files[0]['filename'];
+    }
+    let email = req.user.email;
+    if (req.body['email'] != '') {
+      email = req.body['email'];
+    }
+    const q = { _id: req.user._id };
+    const newData = { email, avatar: imagePath };
+    User.findByIdAndUpdate(
+      q,
+      newData,
+      { useFindAndModify: false },
+      (err, doc) => {
+        if (err) return send422(res);
+        return res.send({ success: 1, avatar: doc.avatar });
+      }
+    );
+  } catch (err) {
+    return send422(res);
+  }
 });
 
 const send422 = res => {
-	res.status(422).send({ error: 'Must provide valid email and password' });
+  res.status(422).send({ error: 'Must provide valid email and password' });
 };
 
 module.exports = router;
