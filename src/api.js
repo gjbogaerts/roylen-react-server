e2f5e9cb53b6ef96b3022af8f97d8dccf2cf12c7
GJ Bogaerts <gj@raker.nl>
2020-04-06 20:55:28 +0200

    moved db to cloud instance

--- src/api.js
+++ src/api.js
@@ -13,18 +13,19 @@ const msgRoutes = require('./routes/MessageRoute');
 const adminRoutes = require('./routes/AdminRoute');
 const DBPASS = require('./env/db');
 
-const mongoUri = `mongodb://roylen:${DBPASS}@localhost/roylen`;
+// const mongoUri = `mongodb://roylen:${DBPASS}@localhost/roylen`;
+const mongoUri = `mongodb+srv://roylen:${DBPASS}@roylen-cluster-x1bzx.azure.mongodb.net/test?retryWrites=true&w=majority`;
 mongoose.connect(mongoUri, {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 mongoose.connection.on('connected', () => {
   console.log('connected to local mongo db server');
 });
-mongoose.connection.on('error', err => {
-  console.error('Error conntecting to local mongdo db server', err);
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to local mongdo db server', err);
 });
 
 //starting app, routes and middleware
@@ -34,7 +35,7 @@ app.get('/', (req, res) => {
 });
 
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Methods',
@@ -53,7 +54,7 @@ app.use(adminRoutes);
 app.use(
   '/uploads',
   express.static(path.join(__dirname, 'uploads'), {
-    extensions: ['jpg', 'jpeg', 'png']
+    extensions: ['jpg', 'jpeg', 'png'],
   })
 );
 app.listen(3000, hostname, () => {
